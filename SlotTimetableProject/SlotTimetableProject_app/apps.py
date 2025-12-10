3964abbc36e36ec448f8abdeace00caaaf0274e4
suzannecassandra1367-gif <suzannecassandra1367@gmail.com>
2025-12-10 11:00:23 +0530

    Success

---
+++ SlotTimetableProject/SlotTimetableProject_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SlottimetableprojectAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SlotTimetableProject_app'
